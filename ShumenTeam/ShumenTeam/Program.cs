8860c207011d6b40d16f4c3265e6f714e8383ebc
ruskovweb <ruskovweb@gmail.com>
2017-05-30 21:59:05 +0300

    Added exercise

---
+++ ShumenTeam/ShumenTeam/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShumenTeam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get the sum of all odd numbers that are divided by 3 and all even numbers that are divisible by 3, 
+            // in range [1...n]
+            // then multiply the result by 3
+            // If the final result is even, print "The result is even number."
+            // else "The result is odd number."
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < n; i++)
+            {
+                // TODO:
+            }
+        }
+    }
+}
